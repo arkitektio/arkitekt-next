5954e6c7b30e0f9a5f2b5ee111d31f317136b345
Johannes Roos <jhnnsrs@gmail.com>
2025-07-24 21:22:26 +0200

    feat: more unit tests

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,4 +1,6 @@
-from mikro_next.api.schema import create_dataset
+from mikro_next.api.schema import create_dataset, from_array_like
+import xarray as xr
+import numpy as np
 from .conftest import AppWithinDeployment
 import pytest
 
@@ -11,3 +13,21 @@ def test_upload(running_app: AppWithinDeployment) -> None:
     )
     
     assert dataset is not None, "Dataset creation failed"
+    
+    
+    
+@pytest.mark.integration
+def test_write_random(running_app: AppWithinDeployment) -> None:
+    """Test writing a random image."""
+    x = from_array_like(
+        xr.DataArray(data=np.random.random((1000, 1000, 10)), dims=["x", "y", "z"]),
+        name="test_random_write",
+    )
+    assert x.id, "Did not get a random rep"
+    assert x.data.shape == (
+        1,
+        1,
+        10,
+        1000,
+        1000,
+    ), "Did not write data according to schema ( T, C, Z, Y, X )"
